4ae23a647025246a7faf6030abc883b79df4ca15
umemasfeera01 <umemasfeera@gmail.com>
2025-11-25 16:46:39 +0500

    new commit

--- thresholds.js
+++ thresholds.js
@@ -2,8 +2,10 @@ import http from 'k6/http'
 import { sleep } from 'k6'
 
 export let options = {
+  vus: 5,
+  duration: "10s",
   thresholds: {
-    http_req_duration: ['p(95)<10', 'p(99)<1500'],
+    http_req_duration: ['p(95)<300', 'p(99)<300'],
   },
 }
 
